1f35a997f40862c1583c879b1f2163306220900a
Timur Melnikov <melnikovt@gmail.com>
2019-07-15 13:37:52 +0300

    lesson 49

--- app/Http/Controllers/DiggingDeeperController.php
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 
@@ -42,13 +43,63 @@ class DiggingDeeperController extends Controller
          */
         $collection = collect($eloquentCollection->toArray());
 
-        dd(
-            get_class($eloquentCollection),
-            get_class($collection),
-            $collection
-        );
+//        dd(
+////            get_class($eloquentCollection),
+////            get_class($collection),
+////            $collection
+////        );
+
+//        $result['first'] = $collection->first();
+//        $result['last'] = $collection->last();
+
+        //dd($result);
+
+        // Выборка
+//        $result['where']['data'] = $collection
+//            ->where('category_id', '=', 10)
+//            ->values()
+//            ->keyBy('id');
+
+        //dd($result);
+
+//        $result['where']['count'] = $result['where']['data']->count();
+//        $result['where']['isEmpty'] = $result['where']['data']->isEmpty();
+//        $result['where']['isNotEmpty'] = $result['where']['data']->isNotEmpty();
+//
+//
+//        $result['first_where'] = $collection->firstWhere('created_at', '>' ,'2019-01-17 01:35:00');
 
 
+        //Мутируем в другой формат
+//        $result['map']['all'] = $collection->map(function (array $item){
+//            $newItem = new \stdClass();
+//            $newItem->item_id = $item['id'];
+//            $newItem->item_name = $item['title'];
+//            $newItem->exists = is_null($item['deleted_at']);
+//            $newItem->xxx = 'yyy';
+//            return $newItem;
+//        });
+
+//        $result['map']['not_exist'] = $result['map']['all']
+//            ->where('exists', '=', false)
+//        ->values()
+//        ->keyBy('item_id');
+
+        //dd($result);
+
+
+        //Мутируем в другой формат саму коллекцию
+        $collection->transform(function (array $item){
+            $newItem = new \stdClass();
+            $newItem->item_id = $item['id'];
+            $newItem->item_name = $item['title'];
+            $newItem->exists = is_null($item['deleted_at']);
+            $newItem->created_at = Carbon::parse($item['deleted_at']);
+            $newItem->xxx = 'yyy';
+            return $newItem;
+        });
+
+        dd($collection);
     }
 
 }
--- docs/lessons.md
+++ docs/lessons.md
@@ -228,3 +228,5 @@ php artisan make:observer BlogCategoryObserver --model=Models\BlogCategory
 48. Laravel урок №48: [ Collections. Коллекции #1 ] - https://www.youtube.com/watch?v=nDcBGssCnH0&list=PLoonZ8wII66iP0fJPHhkLXa3k7CMef9ak&index=53
 
 49. Laravel урок №49: [ Collections. Коллекции #2 ] - https://www.youtube.com/watch?v=zkuAcfdnS48&list=PLoonZ8wII66iP0fJPHhkLXa3k7CMef9ak&index=54
+
+50. Laravel урок №50: [ Collections. Коллекции #3 ] - https://www.youtube.com/watch?v=-zRCx5K82QA&list=PLoonZ8wII66iP0fJPHhkLXa3k7CMef9ak&index=55
